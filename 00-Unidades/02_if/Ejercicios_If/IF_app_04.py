750a23326ea02df3b015b5c746c84de9d16f2d66
Julieta <perezmedranojulieta@gmail.com>
2024-02-09 14:36:01 -0300

    IF ejercicio 4

--- 00-Unidades/02_if/Ejercicios_If/IF_app_04.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_04.py
@@ -34,6 +34,10 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
+        edad = int(self.txt_edad.get())
+
+        if edad >= 13 and edad <= 17:
+            alert(message="Es adolescente")
         pass
 
 

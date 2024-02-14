6d5d234d31d2edf083151d2bf48286b3cc404750
Julieta <perezmedranojulieta@gmail.com>
2024-02-14 15:09:01 -0300

    MATCH ejercicio 7

--- 00-Unidades/03_match/Ejercicios_Match/Match_app_07.py
+++ 00-Unidades/03_match/Ejercicios_Match/Match_app_07.py
@@ -6,8 +6,8 @@ import customtkinter
 
 
 '''
-nombre:
-apellido:
+nombre: Julieta 
+apellido: Perez Medrano
 ---
 Ejercicio: Match_07
 ---
@@ -34,7 +34,19 @@ class App(customtkinter.CTk):
         
     
     def btn_informar_on_click(self):
-        pass
+        destino = self.combobox_destino.get()
+
+        match destino:
+            case "Bariloche":
+                respuesta = "Oeste"
+            case "Mar del plata":
+                respuesta = "Este"
+            case "Cataratas":
+                respuesta = "Norte"
+            case "Ushuaia":
+                respuesta = "Sur"
+
+        alert(message=respuesta)
     
     
 if __name__ == "__main__":
